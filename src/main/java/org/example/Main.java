ee4d48f0970e3ceaa1ab7fe74efd17f1c07e5d2f
シ　チケン <shizhixuan@shichikennoMacBook-Air.local>
2023-12-10 22:01:17 +0900

    第3回課題を作成

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> userMap = new HashMap<>();
+        userMap.put("koyama", "I am Koyama. I live in Tokyo");
+        userMap.put("sato", "I am sato. I live in Osaka");
+        userMap.put("tanaka", "I am tanaka. I live in Nagoya");
+
+        System.out.println(userMap.get("koyama"));
+        System.out.println(userMap.get("sato"));
+        System.out.println(userMap.get("tanaka"));
+
+        boolean havingkoyama = userMap.containsKey("koyama");
+        System.out.println("koyama　は存在しますか");
+        if (havingkoyama == true) {
+            System.out.println("はい");
+        } else {
+            System.out.println("いいえ");
+        }
+
+//        List<Student> studentList = List.of(new Student("⼭⽥", 16, "A"),
+//                new Student("⽥中", 17, "B"),
+//                new Student("鈴⽊", 15, "A"));
+//        for (Student student : studentList) {
+//            if (student.getClassName().equals("A")) {
+//                System.out.println("学⽣の名前：" + student.getName());
+//            }
+//        }
+
+
+    }
+}
+
+//        for (int i = 0; i <10; i++){
+//            System.out.println(i + "番目");
+//        }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Student {
+    private String name;
+    private int age;
+    private String getClassName;
+    public Student(String name, int age, String getClassName) {
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getClassName() {
+        return getClassName;
+    }
+}
